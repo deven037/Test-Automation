32f138e2ae828829b857c2e7f4f7fe4f9e67bf15
deven-choudhary <devenchoudhary037@gmail.com>
2025-12-30 22:08:29 +0530

    fix Ci/CD

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,32 +2,41 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private static WebDriver driver;
 
-    public static void initDriver(String browser) {
-        if (browser.equalsIgnoreCase("chrome")) {
-            driver.set(new ChromeDriver());
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            driver.set(new FirefoxDriver());
+    public static WebDriver initDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // Detect if running in CI/CD
+        String ci = System.getenv("CI");
+        if (ci != null && ci.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new"); // Headless mode for CI
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
         } else {
-            throw new RuntimeException("Browser not supported: " + browser);
+            options.addArguments("--start-maximized");
         }
 
-        getDriver().manage().window().maximize();
+        driver = new ChromeDriver(options);
+        return driver;
     }
 
     public static WebDriver getDriver() {
-        return driver.get();
+        return driver;
     }
 
     public static void quitDriver() {
-        if (getDriver() != null) {
-            getDriver().quit();
-            driver.remove();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
         }
     }
 }
