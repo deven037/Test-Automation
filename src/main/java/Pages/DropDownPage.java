1a2d447999c05adae7ef35f2dd2215f52f7bb581
deven-choudhary <devenchoudhary037@gmail.com>
2026-01-04 19:24:24 +0530

    fix page

--- src/main/java/Pages/DropDownPage.java
+++ src/main/java/Pages/DropDownPage.java
@@ -28,8 +28,8 @@ public class DropDownPage extends BasePage {
 	private By successMsg = By.xpath("/html/body/div[3]/div/div/div[2]/p");
 	
 	public void selectPrefix(String prefixText) {
-		wait.waitForClickability(prefix);
-		click(prefix);
+//		wait.waitForInteractableAndClick(prefix);
+//		click(prefix);
 		selectFromDropDown(prefix, prefixText);
 	}
 	
