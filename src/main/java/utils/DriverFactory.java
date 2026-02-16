0dcd06aa5a02e65545691db463626d7fecd246b1
deven-choudhary <devenchoudhary037@gmail.com>
2026-02-16 20:07:50 +0530

    fix CI issue

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,6 +3,7 @@ package utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -43,12 +44,19 @@ public class DriverFactory {
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
             options.addArguments("--window-size=1920,1080");
+            options.addArguments("--force-device-scale-factor=1");
         } else {
             options.addArguments("--start-maximized");
         }
 
         WebDriver driverInstance = new ChromeDriver(options);
         driver.set(driverInstance);
+
+        if (ci != null && ci.equalsIgnoreCase("true")) {
+            driverInstance.manage().window().setSize(new Dimension(1920, 1080));
+        } else {
+            driverInstance.manage().window().maximize();
+        }
 
         return driverInstance;
     }
